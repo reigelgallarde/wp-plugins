1a54d0c9c45e1e34a37cc3ff865226c5c705b794
Reigel Gallarde <reigelgallarde@gmail.com>
2015-06-19 18:33:11 +0900

    Update widget-bug-fix.js

--- customizer-widget-bug-fix/widget-bug-fix.js
+++ customizer-widget-bug-fix/widget-bug-fix.js
@@ -1,20 +1,21 @@
 jQuery(document).ready( function($) {
-
+	
 	$( document ).on( 'widget-added', function(event, widget) {
-
 		var $saveBtn = $(widget).find('.widget-control-save');
 		// let's check if the control for this widget is ready... 
 		if ($saveBtn.closest('.control-section').is(':visible')) {
 			// if the control for this widget is ready, let's hit apply...
 			$saveBtn.click();
+			widget.data('new_widget', true);
 		}
 	});
-  // update the content of the widget....
+	
+	// update the content of the widget....
 	$( document ).on( 'widget-synced', function(e,widget ,form) {
 		var _widget = $('#' + widget[0].id);
-		if (_widget.data('synced') != true) {
+		if (_widget.data('new_widget') == true) {
 			_widget.find('.widget-content').html(form);
-			_widget.data('synced', true);	
+			_widget.data('new_widget', false);	
 		}
 	});
 
